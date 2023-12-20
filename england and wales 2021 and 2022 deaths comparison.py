37507680e02a9563a6af5c004c13897be40d49fb
zoro <148241935+xorujuro@users.noreply.github.com>
2023-12-20 17:58:20 +0530

    Update england and wales 2021 and 2022 deaths comparison.py

--- england and wales 2021 and 2022 deaths comparison.py
+++ england and wales 2021 and 2022 deaths comparison.py
@@ -1,20 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Assuming you have a DataFrame called 'df' with columns 'Week_number', 'Total_deaths_England_and_Wales_2021', and 'Total_deaths_England_and_Wales_2022'
-# Replace 'your_dataset.csv' with the actual file or provide your data in another way
 
 df = pd.read_csv('/content/deaths_covid.csv')
 
-# Convert columns to float
 df['Total_deaths_England_and_Wales_2021'] = df['Total_deaths_England_and_Wales_2021'].astype(str).str.replace(',', '').astype(float)
 df['Total_deaths_England_and_Wales_2022'] = df['Total_deaths_England_and_Wales_2022'].str.replace(',', '').astype(float)
 
-# Group by 'Week_number' and calculate the mean
 mean_2021 = df.groupby('Week_number')['Total_deaths_England_and_Wales_2021'].mean()
 mean_2022 = df.groupby('Week_number')['Total_deaths_England_and_Wales_2022'].mean()
 
-# Plot the bar graph
 plt.figure(figsize=(12, 6))
 bar_width = 0.35
 bar_positions_2021 = mean_2021.index - bar_width / 2
